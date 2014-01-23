10b536407bf30b3367021c6c79d084f7455c2b17
sla89 <skullsplitter@die-optimisten.net>
2014-01-23 11:59:58 +0100

    init?

--- Semaphore/main.c
+++ Semaphore/main.c
@@ -1,24 +1,21 @@
 #include <msp430.h> 
 
-/*
- * main.c
- */
-int main(void) {
-	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-	WDTCTL = WDT_ADLY_1000; // WDT 1s interval timer
-	IE1 |= WDTIE; // Enable WDT interrupt
+void main(void) {
+	WDTCTL = WDTPW + WDTHOLD;	// Stop watchdog timer
+	P1DIR |= BIT0; 				// Set P1.0 to output direction
 
-	_BIS_SR(GIE);
-
-	while (1) {
-	}
-
+	TA0CCR0 = 12000;			// Count limit (16 bit)
+	TA0CCTL0 = 0x10;			// Enable counter interrupts, bit 4=1
+	TA0CTL = TASSEL_1 + MC_1; 	// Timer A 0 with ACLK @ 12KHz, count UP
+	_BIS_SR(LPM0_bits + GIE); 	// LPM0 (low power mode) with interrupts enabled
 }
 
-#pragma vector=WDT_VECTOR
-__interrupt void watchdog_timer(void) {
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer0_A0(void) {		// Timer0 A0 interrupt service routine
 
+	P1OUT ^= BIT0;						// Toggle red LED
 }
+
 
 typedef struct {
 	int id=0;
