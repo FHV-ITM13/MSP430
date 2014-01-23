1c6056944dbcd5dc0060f6d0833b0b261d9ccdde
sla89 <skullsplitter@die-optimisten.net>
2014-01-23 22:28:31 +0100

    added led functionallity (thread functions)

--- OS_Semaphoren/src/main.c
+++ OS_Semaphoren/src/main.c
@@ -1,9 +1,12 @@
 #include <msp430.h> 
 #include "scheduler/scheduler.h"
 
+void turnLedOn();
+void turnLedOff();
+
 void main(void) {
 	WDTCTL = WDTPW + WDTHOLD;	// Stop watchdog timer
-	P1DIR |= BIT0; 				// Set P1.0 to output direction
+	P1DIR |= 0x41;				// Set P1.0  and P1.6 to output direction
 
 	TA0CCR0 = 12000;			// Count limit (16 bit)
 	TA0CCTL0 = 0x10;			// Enable counter interrupts, bit 4=1
@@ -12,8 +15,8 @@ void main(void) {
 
 	scheduler_init();
 
-//	scheduler_startThread();
-//	scheduler_startThread();
+	scheduler_startThread((*turnLedOn));
+	scheduler_startThread((*turnLedOff));
 
 	while(1) {
 
@@ -22,5 +25,16 @@ void main(void) {
 
 #pragma vector=TIMER0_A0_VECTOR
 __interrupt void Timer0_A0(void) {		// Timer0 A0 interrupt service routine
+	//change threads each time cycle
 	scheduler_runNextThread();
+}
+
+void turnLedOn() {
+	P1OUT ^= 0x41;				// Toggle P1.0 and P1.6 using exclusive-OR
+
+}
+
+void turnLedOff() {
+	P1OUT ^= 0x41;				// Toggle P1.0 and P1.6 using exclusive-OR
+
 }
